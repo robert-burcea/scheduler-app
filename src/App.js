c9435aee2e8644af8a9813e8d01c655adb3f33a2
robert <robert.burcea@gmail.com>
2023-02-08 19:56:04 +0200

    firebase

--- src/App.js
+++ src/App.js
@@ -92,14 +92,14 @@ function App() {
   /* **********TODOIST FUNCTIONS************* */
 
   const fetchAllData = () => {
-    fetch(`https://main.d1yx0xm55m2bff.amplifyapp.com:5000/api/todoist/`, {
+    fetch(`https://scheduler-app-v2.web.app:5000/api/todoist/`, {
       headers: {
         'Authorization':`${todoistApiKey}`
       }
     })
     .then((response) => response.json())
     .then((todoistData) => {
-      fetch(`https://main.d1yx0xm55m2bff.amplifyapp.com:5000/api/toggl/`, {
+      fetch(`https://scheduler-app-v2.web.app:5000/api/toggl/`, {
         headers: {
           'Authorization':`${togglApiKey}`
         }
